d5be0f5de799af9ce94d1deb573cf2a58dfa22df
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-07-06 06:57:37 +0300

    CountDownTimer как работает таймер ...

--- app/src/main/java/com/omelchenkoaleks/braintrainer/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/braintrainer/MainActivity.java
@@ -4,21 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTimerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences preferences =
-                PreferenceManager.getDefaultSharedPreferences(this);
-//        preferences.edit().putInt("test", 5).apply();
-        int test = preferences.getInt("test", 0);
-        Toast.makeText(this, Integer.toString(test),
-                Toast.LENGTH_SHORT).show();
+        mTimerTextView = findViewById(R.id.timer_text_view);
+
+        CountDownTimer timer = new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                mTimerTextView.setText(Integer.toString(seconds));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+
+        timer.start();
     }
 }
